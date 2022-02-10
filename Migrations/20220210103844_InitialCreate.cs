1eb72658e24098752d4b8ed37ec0d268ec29a12b
An tran <jimmytran411@gmail.com>
2022-02-10 12:47:14 +0200

    add db models

---
+++ Migrations/20220210103844_InitialCreate.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TodoApp.Models;
+
+namespace TodoApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:status_types", "not_started,on_going,completed");
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Todo_item",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Created_At = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Updated_At = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<StatusTypes>(type: "status_types", nullable: false, defaultValue: "not_started"),
+                    User_Id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todo_item", x => x.Id);
+                    table.ForeignKey(
+                        name: "User_id",
+                        column: x => x.User_Id,
+                        principalTable: "User",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todo_item_User_Id",
+                table: "Todo_item",
+                column: "User_Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todo_item");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
---
+++ Models/TodoAppContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Npgsql;
+using TodoApp.Models;
+
+#nullable disable
+
+namespace TodoApp.Models
+{
+    public partial class TodoAppContext : DbContext
+    {
+         static TodoAppContext()
+        => NpgsqlConnection.GlobalTypeMapper.MapEnum<StatusTypes>();
+
+        public TodoAppContext()
+        {
+        }
+
+        public TodoAppContext(DbContextOptions<TodoAppContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TodoItem> TodoItems { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Name=ConnectionStrings:PostgresConnection");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasPostgresEnum<StatusTypes>();
+
+            modelBuilder.Entity<TodoItem>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.TodoItems)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("User_id");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+       
+    }
+}
